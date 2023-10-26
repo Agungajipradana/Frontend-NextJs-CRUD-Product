7216dff466b70de17609bae65643d42c2e6ebb60
Agung Aji Pradana <agungajipradana@gmail.com>
2023-10-26 23:37:38 +0700

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,35 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const theme = extendTheme({
+  colors: {
+    cyan: {
+      50: "#E0F7FA",
+      100: "#B2EBF2",
+      // Tambahkan nilai warna sesuai kebutuhan Anda
+    },
+    red: {
+      50: "#FFEBEE",
+      100: "#FFCDD2",
+      // Tambahkan nilai warna sesuai kebutuhan Anda
+    },
+  },
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
+}
